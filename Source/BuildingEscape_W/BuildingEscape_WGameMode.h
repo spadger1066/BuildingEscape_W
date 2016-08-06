274c63e89b8289c36e04873efc71aed8ead5d17d
RAMPAGE\Stephen <s.maitland@me.com>
2016-08-06 17:37:33 +0100

    Initial commit

---
+++ Source/BuildingEscape_W/BuildingEscape_WGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BuildingEscape_WGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE_W_API ABuildingEscape_WGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
